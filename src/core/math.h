bbcfdb01065f98e4b0afe6b5fc83d2e556478044
lhabacuc <lisvaldosf@gmail.com>
2026-09-15 12:30:35 +0100

    2026-09-15 12:30:35

--- src/core/math.h
+++ src/core/math.h
@@ -4,5 +4,8 @@
 # include "tensor.h"
 
 Tensor	add(const Tensor& a, const Tensor& b);
+Tensor	subtract(const Tensor& a, const Tensor& b);
+Tensor	multiply(const Tensor& a, const Tensor& b);
+Tensor	scalar_multiply(const Tensor& tensor, float scalar);
 
 #endif
